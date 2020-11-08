a5a4ca3f024ef2517db7e6a8827189b933c363f1
Abijith <abijithks@gmail.com>
2020-11-08 01:29:54 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,12 @@
 from winreg import *
 from flask import Flask, request, jsonify
+from threading import Timer
 
 app = Flask(__name__)
 keyVal = r"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options"
 
-@app.route('/shutoff', methods = ['POST'])
-def shutoff():
-   data = request.json # a multidict containing POST data
-   for i in data["executables"]:
+def start(lst):
+   for i in lst:
       regkey = keyVal+"\\"+i
       try:
          key = OpenKey(HKEY_LOCAL_MACHINE, regkey, 0, KEY_ALL_ACCESS)
@@ -16,19 +15,27 @@ def shutoff():
       SetValueEx(key, "Debugger", 0, REG_SZ, "ntsd -c q")
       print(regkey)
 
+def end(lst):
+   for i in lst:
+      regkey = keyVal+"\\"+i
+      try:
+         key = OpenKey(HKEY_LOCAL_MACHINE, regkey, 0, KEY_ALL_ACCESS)
+         DeleteValue(key, "Debugger") 
+      except:
+         pass
+      print(regkey)
+
+@app.route('/shutoff', methods = ['POST'])
+def shutoff():
+   data = request.json # a multidict containing POST data
+   start(data["executables"])
+   t = Timer(data['time'], end, args = (data["executables"],))
+   t.start()
    return jsonify(data)
 
 
 @app.route('/restart', methods = ['POST'])
 def restart():
    data = request.json # a multidict containing POST data
-   for i in data["executables"]:
-      regkey = keyVal+"\\"+i
-      try:
-         key = OpenKey(HKEY_LOCAL_MACHINE, regkey, 0, KEY_ALL_ACCESS)
-      except:
-         key = CreateKey(HKEY_LOCAL_MACHINE, regkey)
-      DeleteValue(key, "Debugger") 
-      print(regkey)
-
+   end(data["executables"])
    return jsonify(data)
